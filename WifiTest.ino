2271e529b30efa56bb88eec04e135fd65cba4976
Brayden <braydenpoon0602@gmail.com>
2026-09-08 18:44:33 -0400

    Add files via upload

---
+++ WifiTest.ino
@@ -0,0 +1,30 @@
+#include <WiFi.h>
+const char* ssid = "ESP32_Hotspot";
+const char* password = "123";
+WiFiServer server(80);
+
+void setup() {
+  Serial.begin(115200);
+  IPAddress IP = WiFi.softAPIP();
+  WiFi.softAP(ssid, password);
+  Serial.print(ssid);
+  Serial.print(password);
+  Serial.print(IP);
+  server.begin();
+}
+
+void loop() {
+  Serial.print("Connected Devices: ");
+  Serial.println(WiFi.softAPgetStationNum());
+  delay(5000);
+  /*WiFiClient client = server.available();
+  if(client){
+    while(client.connected()){
+      if(client.available()){
+        char c = client.read();
+        Serial.write(c);
+      }
+    }
+  }
+  */
+}
